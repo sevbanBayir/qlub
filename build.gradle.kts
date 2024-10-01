3e816d4ffb584b7ab8c752a631e78125552aab64
Sevban Bayır <sevbanbuyer@gmail.com>
2024-10-02 02:13:58 +0300

    feature: created model module

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,4 +5,5 @@ plugins {
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlinKapt) apply false
     alias(libs.plugins.kotlin.ksp) apply false
+    alias(libs.plugins.jetbrains.kotlin.jvm) apply false
 }
