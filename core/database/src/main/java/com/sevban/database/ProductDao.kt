1ca8b57c6b2399e2e2a09632f9c73d85f5d09e88
Sevban Bayır <sevbanbuyer@gmail.com>
2024-10-02 02:02:05 +0300

    feature: created ProductsDatabase.kt & dao for it

---
+++ core/database/src/main/java/com/sevban/database/ProductDao.kt
@@ -0,0 +1,21 @@
+package com.sevban.database
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.sevban.database.entity.ProductEntity
+
+@Dao
+interface ProductDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertProducts(productEntities: List<ProductEntity>)
+
+    // Query Operations
+    @Query("SELECT * FROM products WHERE id = :productId")
+    suspend fun getProductById(productId: Int): ProductEntity
+
+    @Query("SELECT * FROM products")
+    suspend fun getProducts(): List<ProductEntity>
+}
