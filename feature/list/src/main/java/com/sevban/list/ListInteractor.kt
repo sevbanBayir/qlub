2e4ccb27e93f91e409bcedf6237f25b820707aff
Sevban Bayır <sevbanbuyer@gmail.com>
2024-10-02 10:10:23 +0300

    feature: created list interactor and view

---
+++ feature/list/src/main/java/com/sevban/list/ListInteractor.kt
@@ -0,0 +1,53 @@
+package com.sevban.list
+
+import com.sevban.common.streams.EventStream
+import com.sevban.common.streams.ItemsStream
+import com.sevban.common.streams.StateStream
+import com.uber.rib.core.BasicInteractor
+import com.uber.rib.core.Bundle
+import com.uber.rib.core.ComposePresenter
+import com.uber.rib.core.coroutineScope
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+
+class ListInteractor(
+    presenter: ComposePresenter,
+    private val itemsStream: ItemsStream,
+    private val eventStream: EventStream<ListEvent>,
+    private val stateStream: StateStream<List<ProductViewModel>>,
+    private val listener: Listener
+) : BasicInteractor<ComposePresenter, ListRouter>(presenter) {
+    override fun didBecomeActive(savedInstanceState: Bundle?) {
+        super.didBecomeActive(savedInstanceState)
+
+        eventStream
+            .observe()
+            .onEach {
+                when (it) {
+                    is ListEvent.OnItemClick -> {
+                        listener.onItemClicked(it.id)
+                    }
+                }
+            }
+            .launchIn(coroutineScope)
+
+        itemsStream
+            .observe()
+            .onEach { items ->
+                val productViewModels = items.map {
+                    ProductViewModel(
+                        id = it.id,
+                        title = it.title,
+                        price = it.price
+                    )
+                }
+                stateStream.dispatch(
+                    productViewModels
+                )
+            }.launchIn(coroutineScope)
+    }
+
+    interface Listener {
+        fun onItemClicked(id: Int)
+    }
+}
