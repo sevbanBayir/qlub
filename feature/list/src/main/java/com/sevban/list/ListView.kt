71e4a8a5b26f241298ba88054883d2aaf1bf4dc6
Sevban Bayır <sevbanbuyer@gmail.com>
2024-10-02 18:58:40 +0300

    feature: create ProductListItem.kt and polish ListView.kt

--- feature/list/src/main/java/com/sevban/list/ListView.kt
+++ feature/list/src/main/java/com/sevban/list/ListView.kt
@@ -1,65 +1,41 @@
 package com.sevban.list
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.ListItem
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.sevban.common.streams.EventStream
+import com.sevban.list.components.ProductListItem
+import com.sevban.ui.theme.QlubTheme
 
 @Composable
 fun ListView(viewModel: State<List<ProductViewModel>>, eventStream: EventStream<ListEvent>) {
     LazyVerticalGrid(
         columns = GridCells.Fixed(2),
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
+        modifier = Modifier.fillMaxSize(),
+        contentPadding = PaddingValues(16.dp),
         verticalArrangement = Arrangement.spacedBy(16.dp, alignment = Alignment.Bottom),
-        horizontalArrangement = Arrangement.spacedBy(
-            16.dp,
-            alignment = Alignment.CenterHorizontally
-        )
+        horizontalArrangement = Arrangement.spacedBy(16.dp, alignment = Alignment.CenterHorizontally)
     ) {
         items(viewModel.value) {
-            ListItem(
-                headlineContent = { Text(it.title) },
-                supportingContent = { Text(it.description) },
-                trailingContent = { Text(it.price) },
-                modifier = Modifier
-                    .size(100.dp)
-                    .clickable { eventStream.notify(ListEvent.OnItemClick(it.id)) }
-                    .clip(RoundedCornerShape(8.dp))
+            ProductListItem(
+                product = it,
+                modifier = Modifier,
+                onClick = {
+                    eventStream.notify(ListEvent.OnItemClick(it.id))
+                }
             )
         }
     }
-}
-
-@Preview
-@Composable
-fun LoggedOutViewPreview() {
-    val viewModel = remember {
-        mutableStateOf(
-            listOf(
-                ProductViewModel(1, "title1", "description1", "1.2"),
-                ProductViewModel(2, "title2", "description2", "2.2"),
-                ProductViewModel(3, "title3", "description3", "3.2"),
-            )
-        )
-    }
-    ListView(viewModel, EventStream())
 }
